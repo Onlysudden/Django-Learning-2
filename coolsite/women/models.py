c2a99801b08af5bafa9eed49abf1dedc9543c66c
Onlysudden <trofimof09@mail.ru>
2022-08-29 14:59:01 +0300

    Finish 8 and 9 lesson

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Women(models.Model):
     title = models.CharField(max_length=255)
@@ -7,6 +8,19 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True) # Создается один раз и не меняется
     time_update = models.DateTimeField(auto_now=True) # Меняется каждый раз при изменении
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey("Category", on_delete=models.PROTECT, null=True) # Category пишем как строку, либо как переменную, но тогда Category должна быть перед Women
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
