e9b748420dc44d8678e66fe8421469ed07cc3dc7
Onlysudden <trofimof09@mail.ru>
2022-09-01 22:29:11 +0300

    Finish 23, 24 lesson

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -8,6 +8,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
+    path('captcha/', include('captcha.urls')),
 ]
 
 if settings.DEBUG:
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
@@ -36,8 +36,19 @@ def about(request):
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def contact(request):
-    return HttpResponse("Обратная связь")
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Обратная связь')
+        return dict(list(context.items()) + list(c_def.items()))
+    
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
